30e09edfe756fdf6b9dee7bccfd92a4c3e6ff09c
Kurosh Hesamshariati <kuroshhesamshariati@gmail.com>
2023-01-02 12:15:01 +0330

    add hover effect

--- script.js
+++ script.js
@@ -1,7 +1,19 @@
+const size = 800;
+let row = 16;
+
 const container = document.querySelector('#container');
 
 for (let index = 0; index < 16*16; index++) {
     const box = document.createElement('div');
-    box.style.cssText = 'width: 50px;height: 50px;border: black 2px solid;'
+    box.style.cssText = `width: ${size/row}px;height: ${size/row}px;border: black 2px solid;background-color: white;`;
+
+    box.onmouseover = function(){
+        box.style.backgroundColor = 'red';
+    }
+
+    box.onmouseout = function () {
+        box.style.backgroundColor = 'white';
+    }
+
     container.appendChild(box);
 }
